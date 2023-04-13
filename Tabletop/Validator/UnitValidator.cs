9c5934fd75c63228bb2c5a0444d9ee50dc7694a8
Tom <support@tablebricks.com>
2023-04-13 17:10:14 +0200

    Add Validator Add UnitForm

---
+++ Tabletop/Validator/UnitValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Tabletop.Models;
+
+namespace Tabletop.Validator
+{
+    public class UnitValidator : AbstractValidator<Unit>
+    {
+        public UnitValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("Name must contain only 20 characters.");
+
+            RuleFor(x => x.Fraction)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("Name must contain only 20 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(255)
+                .WithMessage("Name must contain only 255 characters.");
+
+            RuleFor(x => x.Defense)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(11)
+                .GreaterThan(0)
+                .WithMessage("Defense value must be between 1 and 10");
+
+            RuleFor(x => x.Moving)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(30)
+                .GreaterThan(0)
+                .WithMessage("Defense value must be between 1 and 30");
+        }
+    }
+}
---
+++ Tabletop/Validator/WeaponValidator.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using System.Xml.Linq;
+using Tabletop.Models;
+
+namespace Tabletop.Validator
+{
+    public class WeaponValidator : AbstractValidator<Weapon>
+    {
+        public WeaponValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("Name must contain only 20 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(255)
+                .WithMessage("Name must contain only 255 characters.");
+
+            RuleFor(x => x.Attack)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(11)
+                .GreaterThan(0)
+                .WithMessage("Attack value must be between 1 and 10");
+
+            RuleFor(x => x.Quality)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(11)
+                .GreaterThan(0)
+                .WithMessage("Defense value must be between 1 and 10");
+
+            RuleFor(x => x.Range)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(120)
+                .GreaterThan(0)
+                .WithMessage("Range value must be between 1 and 120");
+
+            RuleFor(x => x.Dices)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(11)
+                .GreaterThan(0)
+                .WithMessage("Dices must be between 1 and 10");
+        }
+    }
+}
