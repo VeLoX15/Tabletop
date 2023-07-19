a3e9e8e95a733791e1a9428811414b714db38f50
Tom | Surface <sassor@tablebricks.com>
2023-07-19 07:33:22 +0200

    Change Footer

--- MYSQL_INSTALL.sql
+++ MYSQL_INSTALL.sql
@@ -112,6 +112,21 @@ CREATE TABLE IF NOT EXISTS `tabletop`.`gamemodes`
 );
 
 -- -----------------------------------------------------
+-- Table `tabletop`.`games`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tabletop`.`games` 
+(
+    `game_id` INTEGER NOT NULL,
+	`gamemode_id` INTEGER NOT NULL,
+	`rounds` INTEGER NOT NULL,
+	`team1_name` VARCHAR(50) NOT NULL,
+	`team2_name` VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (`game_id`),
+	FOREIGN KEY (`gamemode_id`) REFERENCES `tabletop`.`gamemodes`(`gamemode_id`)
+);
+
+-- -----------------------------------------------------
 -- Table `tabletop`.`user_units`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `tabletop`.`user_units` 
@@ -136,4 +151,34 @@ CREATE TABLE IF NOT EXISTS `tabletop`.`user_friends`
     PRIMARY KEY (`user_id`, `friend_id`),
 	FOREIGN KEY (`user_id`) REFERENCES `tabletop`.`users`(`user_id`),
 	FOREIGN KEY (`friend_id`) REFERENCES `tabletop`.`users`(`user_id`)
+);
+
+-- -----------------------------------------------------
+-- Table `tabletop`.`game_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tabletop`.`game_users` 
+(
+	`game_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+	`fraction_id` INTEGER NOT NULL,
+	`team` INTEGER NOT NULL,
+
+    PRIMARY KEY (`game_id`, `user_id`),
+	FOREIGN KEY (`game_id`) REFERENCES `tabletop`.`games`(`game_id`)
+	FOREIGN KEY (`user_id`) REFERENCES `tabletop`.`users`(`user_id`),
+);
+
+-- -----------------------------------------------------
+-- Table `tabletop`.`user_game_unit`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tabletop`.`game_user_unit` 
+(
+	`game_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+	`unit_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`game_id`, `user_id`, `unit_id`),
+	FOREIGN KEY (`game_id`) REFERENCES `tabletop`.`games`(`game_id`),
+	FOREIGN KEY (`user_id`) REFERENCES `tabletop`.`users`(`user_id`),
+	FOREIGN KEY (`unit_id`) REFERENCES `tabletop`.`units`(`unit_id`)
 );
